f059c6c84f8a3e82525ae510a12d6790e0b473bd
Ruslan <yarema2003r@gmail.com>
2024-02-19 16:29:11 +0200

    test

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function showMessage(response) {
 
     // Update text content and hide name message
     document.getElementById("question").textContent =
-      "The no button is just for visuals";
+      "Кнопка Ні тут для прикола.. Ти приколіст?";
     document.getElementById("name").style.display = "none";
 
     // Optional: You can also add a timeout to reset the position after a few seconds
